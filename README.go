14dc124650b23fc236e4c6e93e21771a9af2d9e8
Ian Denhardt <ian@zenhack.net>
2016-04-24 11:07:08 -0400

    Move README into a go source file.
    ...lets us double up with package docs.

---
+++ README.go
@@ -0,0 +1,17 @@
+// Package `fmtmail` builds on top of the standard library's `net/mail`, by
+// adding a single function:
+//
+//     func WriteMessage(w io.Writer, msg *mail.Message) error
+//
+// ...Which outputs the message to `w`.
+//
+// The basic functionality already works, but there are still some details
+// to finish up:
+//
+// * Handle outputting "structured" fields; we can't just split everything
+//   on character boundaries.
+// * Go over RFC 5322 and make sure we're hitting all of the edge cases.
+//   Right now we're probably missing some important stuff.
+//
+// Released under a simple permissive license, see `COPYING`.
+package fmtmail
--- fmtmail.go
+++ fmtmail.go
@@ -1,17 +1,3 @@
-// Package `fmtmail` builds on top of the standard library's `net/mail`, by
-// adding a single function:
-//
-//     func WriteMessage(w io.Writer, msg *mail.Message) error
-//
-// ...Which outputs the message to `w`.
-//
-// The basic functionality already works, but there are still some details
-// to finish up:
-//
-// * Handle outputting "structured" fields; we can't just split everything
-//   on character boundaries.
-// * Go over RFC 5322 and make sure we're hitting all of the edge cases.
-//   Right now we're probably missing some important stuff.
 package fmtmail
 
 import (
