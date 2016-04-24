913839f0b26b4ddeedf40ea46f5f2d54ea16e87e
Ian Denhardt <ian@zenhack.net>
2016-04-24 10:57:02 -0400

    docs/license

--- fmtmail.go
+++ fmtmail.go
@@ -1,3 +1,17 @@
+// Package `fmtmail` builds on top of the standard library's `net/mail`, by
+// adding a single function:
+//
+//     func WriteMessage(w io.Writer, *mail.Message) error
+//
+// ...Which outputs the message to `w`.
+//
+// The basic functionality already works, but there are still some details
+// to finish up:
+//
+// * Handle outputting "structured" fields; we can't just split everything
+//   on character boundaries.
+// * Go over RFC 5322 and make sure we're hitting all of the edge cases.
+//   Right now we're probably missing some important stuff.
 package fmtmail
 
 import (
@@ -8,6 +22,12 @@ import (
 	"strings"
 )
 
+// Write msg to w. An error will be returned if:
+//
+// 1. The writer returns an error during the course of writing.
+// 2. The message contains characters that cannot be encoded
+//    (mainly non-ascii in the headers). Determining what escaping
+//    the standards allow and implementing it is still TODO
 func WriteMessage(w io.Writer, msg *mail.Message) (err error) {
 	for k, v := range msg.Header {
 		for i := range v {
@@ -25,6 +45,7 @@ func WriteMessage(w io.Writer, msg *mail.Message) (err error) {
 	return err
 }
 
+// Write a single header key: value pair.
 func writeHeader(w io.Writer, k string, v string) (err error) {
 	if _, err = w.Write([]byte(k)); err != nil {
 		return err
@@ -35,6 +56,9 @@ func writeHeader(w io.Writer, k string, v string) (err error) {
 	return writeHeaderValue(len(k)+1, w, v)
 }
 
+// Write the value part of a header. `cols` is the starting offset from the
+// beginning of the line, e.g. if the header is "To", then cols will be 3
+// (two characters plus the colon).
 func writeHeaderValue(cols int, w io.Writer, v string) (err error) {
 	r := strings.NewReader(v)
 	buf := &bytes.Buffer{}
